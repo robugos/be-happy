3dac1b60b92971846c9f3fa4afb0d896b196268a
mauriokk@gmail.com <mauriokk@gmail.com@2a69e7f9-2ffd-4aa7-3319-d6d72bed1578>
2013-11-26 19:57:53 +0000

    Sprint 1 - Método de tocar música

--- gen/com/example/behappy/R.java
+++ gen/com/example/behappy/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int amarelo=0x7f020000;
@@ -31,17 +31,17 @@ public final class R {
         public static final int vermelho=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int btnDiagnostico=0x7f080009;
-        public static final int btnNoticia=0x7f080008;
-        public static final int ibtnAmarelo=0x7f080000;
-        public static final int ibtnAzul=0x7f080001;
-        public static final int ibtnDesafio=0x7f080006;
-        public static final int ibtnEducacional=0x7f080007;
-        public static final int ibtnGame=0x7f080005;
-        public static final int ibtnVerde=0x7f080002;
-        public static final int ibtnVermelho=0x7f080003;
-        public static final int listaFonte=0x7f080004;
+        public static final int action_settings=0x7f09000a;
+        public static final int btnDiagnostico=0x7f090009;
+        public static final int btnNoticia=0x7f090008;
+        public static final int ibtnAmarelo=0x7f090000;
+        public static final int ibtnAzul=0x7f090001;
+        public static final int ibtnDesafio=0x7f090006;
+        public static final int ibtnEducacional=0x7f090007;
+        public static final int ibtnGame=0x7f090005;
+        public static final int ibtnVerde=0x7f090002;
+        public static final int ibtnVermelho=0x7f090003;
+        public static final int listaFonte=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -49,24 +49,27 @@ public final class R {
         public static final int home=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+    }
+    public static final class raw {
+        public static final int test=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int amarelo=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int azul=0x7f050005;
-        public static final int desafio=0x7f05000a;
-        public static final int diagnostico=0x7f05000d;
-        public static final int educacional=0x7f05000b;
-        public static final int fonte=0x7f050008;
-        public static final int game=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int home=0x7f05000e;
-        public static final int noticia=0x7f05000c;
-        public static final int sfonte=0x7f050007;
-        public static final int verde=0x7f050004;
-        public static final int vermelho=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int amarelo=0x7f060003;
+        public static final int app_name=0x7f060000;
+        public static final int azul=0x7f060005;
+        public static final int desafio=0x7f06000a;
+        public static final int diagnostico=0x7f06000d;
+        public static final int educacional=0x7f06000b;
+        public static final int fonte=0x7f060008;
+        public static final int game=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int home=0x7f06000e;
+        public static final int noticia=0x7f06000c;
+        public static final int sfonte=0x7f060007;
+        public static final int verde=0x7f060004;
+        public static final int vermelho=0x7f060006;
     }
     public static final class style {
         /** 
@@ -90,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
