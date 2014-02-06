bcbb611ff3dc06f472fa6ff4a84db0b957db2a6d
larissakarollyne@gmail.com <larissakarollyne@gmail.com@2a69e7f9-2ffd-4aa7-3319-d6d72bed1578>
2014-02-06 03:52:42 +0000

    Adaptação a renderização - telas fixas

--- trunk/BeHappy/src/mod/educacional/Banheiro.java
+++ trunk/BeHappy/src/mod/educacional/Banheiro.java
@@ -2,12 +2,15 @@ package mod.educacional;
 
 import com.example.behappy.R;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Banheiro extends Activity {
 	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.banheiro);
+	public void onCreate(Bundle savedInstanceState)
+	{
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.banheiro);
+	this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}
 }
--- trunk/BeHappy/src/mod/educacional/Sono.java
+++ trunk/BeHappy/src/mod/educacional/Sono.java
@@ -2,6 +2,7 @@ package mod.educacional;
 
 import com.example.behappy.R;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Sono extends Activity {
@@ -9,5 +10,6 @@ public class Sono extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sono);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}
 }
